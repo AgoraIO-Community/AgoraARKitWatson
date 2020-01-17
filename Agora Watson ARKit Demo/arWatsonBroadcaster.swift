c735e2a9d010afad2e996f305e84a2f460155b92
Hermes <hermes.frangoudis@gmail.com>
2020-01-17 17:13:56 -0500

    updated ignore list

---
+++ Agora Watson ARKit Demo/arWatsonBroadcaster.swift
@@ -0,0 +1,9 @@
+//
+//  arWatsonBroadcaster.swift
+//  Agora Watson ARKit Demo
+//
+//  Created by Hermes Frangoudis on 1/17/20.
+//  Copyright © 2020 Agora.io. All rights reserved.
+//
+
+import Foundation
